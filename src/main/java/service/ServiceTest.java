ea5c4ae9bcd45119658a8a2690520d7ae4618f70
Andreja <andreja.sambolec@splendit.at>
2018-03-28 13:08:59 +0200

    change

--- src/main/java/service/ServiceTest.java
+++ src/main/java/service/ServiceTest.java
@@ -2,9 +2,14 @@ package service;
 
 public class ServiceTest {
 
+	static
+	int i = 
+			1;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        
+        i = i+1;
     }
 }
 
